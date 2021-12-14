cf922936538b6256017358aadf68538b9d92c162
_vitormuller <vitormuller66@gmail.com>
2021-12-14 15:56:21 -0300

    feat: layout telas 1 e 2

---
+++ src/TelaHorario.js
@@ -0,0 +1,43 @@
+export default function TelaHorario(){
+    return(
+        <>
+        <div class="topo">
+        CINEFLEX
+      </div>
+      <div class="selecao">
+        Selecione o horário
+      </div>
+
+      <div class="horarioFilme">
+        <p>Quinta-feira - 24/06/2021</p>
+        <div class="listaHorario">
+          <div class="horario">
+            15:00
+          </div>
+          <div class="horario">
+            19:00
+          </div>
+        </div>
+      </div>
+
+      <div class="horarioFilme">
+        <p>Quinta-feira - 24/06/2021</p>
+        <div class="listaHorario">
+            <div class="horario">
+              15:00
+            </div>
+            <div class="horario">
+              19:00
+            </div>
+        </div>
+      </div>
+      <div class="rodape">
+        <div class="filmeRodape">
+          a
+        </div>
+        <p>Enola Holmes</p>
+      </div>
+        
+        </>
+    )
+}
---
+++ src/TelaInicio.js
@@ -0,0 +1,38 @@
+export default function TelaInicio(){
+    return(
+        <>
+        <div class="topo">
+            CINEFLEX
+        </div>
+        <div class="selecao">
+            Selecione o filme
+        </div>
+        <div class="listaFilmes">
+            <div class="filme">
+            a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+            <div class="filme">
+                a
+            </div>
+        </div>
+    </>
+    )
+}
